ac0af19559463b1409e9b7469127f076f54fbb09
VIKCA-1 <vikk1ka@yandex.ru>
2026-05-18 11:38:45 +0300

    Добавление MatrixClient с логином

--- mainwidget.h
+++ mainwidget.h
@@ -5,18 +5,22 @@
 #include <QLineEdit>
 #include <QLabel>
 
+class MatrixClient;
+
 class MainWidget : public QWidget {
     Q_OBJECT
 public:
     MainWidget(QWidget *parent = nullptr);
 private slots:
     void LoginCl();
+    void LoginOk(const QString& token, const QString& uid);
 private:
     QLineEdit* serverIn;
     QLineEdit* loginIn;
     QLineEdit* passIn;
     QPushButton* loginBtn;
     QLabel* statusLabel;
+    MatrixClient* cl;
 };
 
 #endif

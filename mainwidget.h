e2483d815763f146b81b7353b9b35d75fc844d5a
VIKCA-1 <vikk1ka@yandex.ru>
2026-05-18 11:39:32 +0300

    Добавление MatrixClient с логином

--- mainwidget.h
+++ mainwidget.h
@@ -1,26 +1,45 @@
-#ifndef MAINWIDGET_H
-#define MAINWIDGET_H
-#include <QWidget>
-#include <QPushButton>
-#include <QLineEdit>
-#include <QLabel>
+#include "matrixclient.h"
 
-class MatrixClient;
+MatrixClient::MatrixClient(QObject *parent) : QObject(parent)
+{
+    nam = new QNetworkAccessManager(this);
+    currReply = nullptr;
+    connect(nam, &QNetworkAccessManager::finished, this, &MatrixClient::replydo);
+}
 
-class MainWidget : public QWidget {
-    Q_OBJECT
-public:
-    MainWidget(QWidget *parent = nullptr);
-private slots:
-    void LoginCl();
-    void LoginOk(const QString& token, const QString& uid);
-private:
-    QLineEdit* serverIn;
-    QLineEdit* loginIn;
-    QLineEdit* passIn;
-    QPushButton* loginBtn;
-    QLabel* statusLabel;
-    MatrixClient* cl;
-};
+void MatrixClient::login(const QString& server, const QString& name, const QString& pass)
+{
+    action = "login";
+    serverUrl = server;
+    QUrl url(serverUrl + "/_matrix/client/v3/login");
+    QNetworkRequest req(url);
+    req.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
 
-#endif
+    QJsonObject json;
+    json["type"] = "m.login.password";
+    json["user"] = name;
+    json["password"] = pass;
+
+    QByteArray data = QJsonDocument(json).toJson();
+    currReply = nam->post(req, data);
+}
+
+void MatrixClient::replydo(QNetworkReply* reply)
+{
+    if (reply != currReply) {
+        reply->deleteLater();
+        return;
+    }
+    QByteArray data = reply->readAll();
+    QJsonDocument doc = QJsonDocument::fromJson(data);
+    QJsonObject json = doc.object();
+
+    if (action == "login") {
+        token = json["access_token"].toString();
+        userId = json["user_id"].toString();
+        emit loginOk(token, userId);
+    }
+
+    currReply = nullptr;
+    reply->deleteLater();
+}
